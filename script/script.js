bfb787e0426701f76c22787dfdb2874a855449de
VikVanDik <95vlc17@gmail.com>
2023-09-12 17:42:42 +0200

    [add]:dice game

--- script/script.js
+++ script/script.js
@@ -10,20 +10,35 @@ console.log (emailList)
 let emailCheck = false;
 
 // Ciclo le mail per controllare che la mail inserita sia all'interno della lista
-for(let i = 0; i < emailList.lenght; i++ ) {
-  const emailSaved = emailList[i]
+for(let i = 0; i < emailList.length; i++ ) {
+  const emailSaved = emailList[i];
   if (eMailUser === emailSaved) {
-    emailCheck = true    
+    emailCheck = true;    
   }
-
-  
 }
 
-// Creo la variabile del messaggio
+// Creo la variabile del messaggio e del gioco
 
 let message = document.getElementById('message')
+const button = document.getElementById('button')
 if (emailCheck === true) {
   message.innerHTML = `Benvenuto al gioco dei dadi`
+  button.classList.remove ("d-none")
+  button.addEventListener('click', function(){
+    const diceUser = Math.floor(Math.random() * 6) +1
+    const dicePC = Math.floor(Math.random() * 6) +1
+    console.log(diceUser)
+    console.log(dicePC)
+
+    const result = document.getElementById('result')
+    if (diceUser === dicePC){
+      result.innerHTML= `Hai estratto ${diceUser} e il pc ha estratto ${dicePC} quindi hai pareggiato`
+    } else if (diceUser > dicePC){
+      result.innerHTML= `Hai estratto ${diceUser} e il pc ha estratto ${dicePC} quindi hai vinto`
+    } else if (diceUser < dicePC){
+      result.innerHTML= `Hai estratto ${diceUser} e il pc ha estratto ${dicePC} quindi hai perso`
+    }
+  })
 } else {
   message.innerHTML = `Inserire una mail registrata`
 }
